e3ccbf9693ec545781c5716cc487ab60d803dac7
MiScarn <bringmenoize@gmail.com>
2023-02-04 01:02:18 +0600

    final deposit amount of bonus

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int initial= 500;
+        int depositAmount = 2000;
+        int percent;
+        if( depositAmount>1000) {
+            percent = 10;
+        } else{
+            percent = 0;
+        }
+        int bonus = depositAmount /100 *percent;
+        System.out.println(initial + depositAmount+ bonus);
+        System.out.println(depositAmount / 100 * percent);
+
+    }
+}
