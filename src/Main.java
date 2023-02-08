d68ae4c6b34b3e6a43ad729dae4c19368a193ae5
MiScarn <bringmenoize@gmail.com>
2023-02-08 18:07:29 +0600

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int initial= 500;
+        int initial = 500;
         int depositAmount = 2000;
         int percent;
-        if( depositAmount>1000) {
+        if (depositAmount > 1000) {
             percent = 10;
-        } else{
+        } else {
             percent = 0;
         }
-        int bonus = depositAmount /100 *percent;
-        System.out.println(initial + depositAmount+ bonus);
+        int bonus = depositAmount / 100 * percent;
+        System.out.println(initial + depositAmount + bonus);
         System.out.println(depositAmount / 100 * percent);
 
     }
